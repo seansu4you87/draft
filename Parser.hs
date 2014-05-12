fb60decfcd60d10cc9f5d12850f36774cf862a31
Sean Yu <seansu4you87@gmail.com>
2014-05-11 23:01:51 -0700

    Adding parsers

--- Parser.hs
+++ Parser.hs
@@ -1,6 +1,18 @@
 module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+             deriving (Show)
+
+instance Show where
+  read val = "hello"
 
 symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=>?@^_~"
@@ -8,12 +20,41 @@ symbol = oneOf "!$%&|*+-/:<=>?@^_~"
 spaces :: Parser ()
 spaces = skipMany1 space
 
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = [first] ++ rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    otherwise -> Atom atom
+
+-- parseNumber :: Parser LispVal
+-- parseNumber = liftM (Number . read) $ many1 digit
+parseNumber :: Parser LispVal
+parseNumber = do
+  number <- many1 digit
+  return $ Number . read $ number
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
+
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
   Left err -> "No match: " ++ show err
-  Right val -> "Found value"
+  Right val -> "Found value: " ++ (read val)
 
 main :: IO ()
-main = do 
+main = do
   args <- getArgs
   putStrLn (readExpr (args !! 0))
